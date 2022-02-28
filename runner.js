d1836867d7b6e1012a6ea0c9a38b38a4b04a0ddf
Mykola Grybyk <mykola.grybyk@gmail.com>
2022-02-28 18:57:14 +0100

    update headers

--- runner.js
+++ runner.js
@@ -106,6 +106,12 @@ const client = axios.create({
   headers: {
     'User-Agent':
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+    'Upgrade-Insecure-Requests': '1',
+    Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': '*',
+    'Cache-Control': 'max-age=0',
+    Connection: 'keep-alive',
   },
 })
 
