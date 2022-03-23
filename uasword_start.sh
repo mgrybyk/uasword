cdea0c031974f9db01ef4134ce957930120fd55b
Mykola Grybyk <mykola.grybyk@gmail.com>
2022-03-23 15:03:50 +0100

    update script

--- uasword_start.sh
+++ uasword_start.sh
@@ -3,7 +3,8 @@
 set -e
 
 cd /opt/uasword
+/usr/bin/git reset --hard
 /usr/bin/git pull --rebase
 /usr/bin/npm i --omit dev --no-fund --no-audit
-/usr/bin/npx playwright install chromium
+/usr/bin/npx playwright install --with-deps chromium
 /usr/bin/node index
